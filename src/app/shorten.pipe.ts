248b66d90170bccb372a3c9162c36f2530969a02
tostos <saittosun2@gmail.com>
2020-05-17 18:05:18 +0200

    parametrizing a custom pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,11 +4,11 @@ import { PipeTransform, Pipe } from '@angular/core';
   name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any) {
+  transform(value: any, limit: number) {
     // tslint:disable-next-line:max-line-length
     // this would be to return and this is important, transform always needs to return something because a pipe is just you put something in, you get something out, so you need to get something out otherwise it won't work.
-    if (value.length > 12) {
-      return value.substr(0, 10) + ' ...';
+    if (value.length > limit) {
+      return value.substr(0, limit) + ' ...';
     }
     return value;
   }
